7cd97d32997e4460588c0702166a644d8490103b
AndyAn6163 <lanews580@gmail.com>
2024-08-09 17:34:29 +0800

    feat: 20240809-77

--- code/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ code/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -4,22 +4,24 @@ export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
-    setIsEditing(true);
+    setIsEditing(!isEditing);
   }
 
   let playerName = <span className="player-name">{name}</span>;
+  // let btnCaption = "Edit";
 
   if (isEditing) {
-    playerName = <input type="text" required />;
+    playerName = <input type="text" required value={name} />;
+    // btnCaption = "Save";
   }
 
   return (
     <li>
-      <span classNmae="player">
+      <span className="player">
         {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
